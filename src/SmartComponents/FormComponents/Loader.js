4b42d73520da61f3e8dd2b6f27a05c8f6b571634
Filip Hlavac <filip.hlavac01@gmail.com>
2020-03-11 12:07:50 +0100

    Add params to Loader component

--- src/SmartComponents/FormComponents/Loader.js
+++ src/SmartComponents/FormComponents/Loader.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Skeleton } from '@redhat-cloud-services/frontend-components';
 import PropTypes from 'prop-types';
 
-const Loader = ({ size }) => (
+// eslint-disable-next-line no-unused-vars
+const Loader = ({ name, size, FieldProvider, formOptions, ...rest }) => (
     <div>
         <Skeleton size={ size }></Skeleton>
     </div>
 );
 
 Loader.propTypes = {
+    FieldProvider: PropTypes.any,
+    formOptions: PropTypes.any,
+    name: PropTypes.string,
     size: PropTypes.string
 };
 
 Loader.defaultProps = {
+    name: '',
     size: 'md'
 };
 
