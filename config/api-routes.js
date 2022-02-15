ddf5ca6bdf9e0f0634130f9f974b72d9065dbfe8
Alexandra Ferdinand <aferdina@redhat.com>
2022-02-15 15:19:57 -0500

    fix route

--- config/api-routes.js
+++ config/api-routes.js
@@ -1,6 +1,7 @@
 const API_PORT = 8085;
 
 const apiRoutes = {
-    '/api/user-preferences/': { host: `http://localhost:${API_PORT}` },
-    '/api/user-preferences/': { host: `http://localhost:${API_PORT}` }
+  '/api/user-preferences/notifications': {
+    host: `http://localhost:${API_PORT}`,
+  },
 };
