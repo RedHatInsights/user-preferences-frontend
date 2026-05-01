71e7aecbb51a86310e04eb3cf83cb368c20e0c57
alex <aferdina@redhat.com>
2026-05-01 10:03:32 -0400

    fix: remove console and xtra fragment

--- src/PresentationalComponents/Notifications/Notifications.js
+++ src/PresentationalComponents/Notifications/Notifications.js
@@ -249,58 +249,52 @@ const Notifications = () => {
               title="My Notifications"
               subtitle={
                 <>
-                  <>
-                    Opt in or out of receiving notifications, and choose how you
-                    want to be notified. Your Organization Administrator has
-                    configured which notifications you can or can{"'"}t receive
-                    on their end.{' '}
-                    <Button
-                      onClick={() => {
-                        if (!vaLoading && Models) {
-                          const [, setState] = vaHookResult || [];
-                          console.log(Models);
-                          setState({
-                            isOpen: true,
-                            currentModel: Models?.VA,
-                            message:
-                              'Contact my organization administrator to update which notifications I receive',
-                          });
-                        }
-                      }}
-                      variant="link"
-                      isInline
-                    >
-                      Contact your Organization Administrator
-                    </Button>{' '}
-                    to have these settings updated.
-                  </>
+                  Opt in or out of receiving notifications, and choose how you
+                  want to be notified. Your Organization Administrator has
+                  configured which notifications you can or can{"'"}t receive on
+                  their end.{' '}
+                  <Button
+                    onClick={() => {
+                      if (!vaLoading && Models) {
+                        const [, setState] = vaHookResult || [];
+                        setState({
+                          isOpen: true,
+                          currentModel: Models?.VA,
+                          message:
+                            'Contact my organization administrator to update which notifications I receive',
+                        });
+                      }
+                    }}
+                    variant="link"
+                    isInline
+                  >
+                    Contact your Organization Administrator
+                  </Button>{' '}
+                  to have these settings updated.
                   <br></br>
-                  <>
-                    {platformNotificationsSeverity && (
-                      <>
-                        <br />
-                        Possible notification severity levels include{' '}
-                        {severityTerms.map((term, index) => (
-                          <React.Fragment key={term.label}>
-                            {index > 0 &&
-                              index < severityTerms.length - 1 &&
-                              ', '}
-                            {index === severityTerms.length - 1 && ', and '}
-                            <SeverityHelpTerm {...term} />
-                          </React.Fragment>
-                        ))}
-                        .{' '}
-                        <a
-                          href={SEVERITY_LEARN_MORE_URL}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          Learn more
-                        </a>
-                        .
-                      </>
-                    )}
-                  </>
+                  {platformNotificationsSeverity && (
+                    <>
+                      <br />
+                      Possible notification severity levels include{' '}
+                      {severityTerms.map((term, index) => (
+                        <React.Fragment key={term.label}>
+                          {index > 0 &&
+                            index < severityTerms.length - 1 &&
+                            ', '}
+                          {index === severityTerms.length - 1 && ', and '}
+                          <SeverityHelpTerm {...term} />
+                        </React.Fragment>
+                      ))}
+                      .{' '}
+                      <a
+                        href={SEVERITY_LEARN_MORE_URL}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Learn more
+                      </a>
+                    </>
+                  )}
                 </>
               }
             >
